77e351aa926a664e917ec1d80088d13a28c6bf6f
Ayaz Ahmad <97458144+iam-ayazahmad@users.noreply.github.com>
2022-07-10 18:54:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import {Navbar} from "./Components/Navbar/Navbar"
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css"
+
+export const Navbar=()=>{
+    return(
+        <div className="mainnav">
+            <div className="icondiv">
+                <img className="icon" src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png" alt="" />
+                <span>Explore</span><span>Plus</span>
+            </div>
+           
+        </div>
+
+    )
+}
