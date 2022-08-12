00797ee34ddbec085873fad59c7f8a2015fb5801
Ayaz Ahmad <97458144+iam-ayazahmad@users.noreply.github.com>
2022-08-12 10:17:10 +0530

    new

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,7 +5,7 @@ export  const Login=()=>{
         <div className="loginback">
             <div className="loginbody">
                 <div className="leftbluelogin">
-                <div><span><span>Login</span></span><p><span>Get access to your Orders, Wishlist and Recommendations</span></p></div>
+                <div><span className="loginwhitext">Login</span><p><span>Get access to your Orders, Wishlist and Recommendations</span></p></div>
                 <div><img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/login_img_c4a81e.png" alt="" /></div>
                 </div>
                 <div className="rightinputlogin"></div>
